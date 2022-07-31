0a2f18a71ef5605d20f1bd9d0d584cea6238bed9
Zach Martin <zach@zach-martin.com>
2022-07-31 04:51:10 -0400

    Added click to move functionality for navigating player

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -1,22 +1,46 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.InputSystem;
 
 public class Mover : MonoBehaviour
 {
     [SerializeField] private Transform target;
     private NavMeshAgent _agent;
-    
-    // Start is called before the first frame update
-    private void Start()
+    private PlayerControls _controls;
+    private Animator _anim;
+    private Camera _cam;
+    private static readonly int IsWalking = Animator.StringToHash("IsWalking");
+
+    private void Awake()
     {
         _agent = GetComponent<NavMeshAgent>();
+        _controls = new PlayerControls();
+        _anim = GetComponent<Animator>();
+        _cam = Camera.main;
     }
 
-    // Update is called once per frame
-    private void Update()
+    // private void Update()
+    // {
+    //     _agent.destination = target.position;
+    // }
+
+    private void OnClickToMove()
     {
-        _agent.destination = target.position;
+        MoveToRay();
+    }
+
+    private void MoveToRay()
+    {
+        var ray = _cam.ScreenPointToRay(Mouse.current.position.ReadValue());
+        var hasHit = Physics.Raycast(ray, out var hit);
+
+        if (!hasHit) return;
+
+        Debug.DrawRay(ray.origin, ray.direction * hit.distance, Color.red, 1f);
+        _agent.destination = hit.point;
+        // _anim.SetBool(IsWalking, true);
     }
 }
